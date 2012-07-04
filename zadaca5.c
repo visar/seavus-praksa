d97244e08c9e57e96b43c335946cd1508d825996
visar <visar.zejnullahu@gmail.com>
2012-07-04 22:32:10 +0200

    Mala modifikacija

--- zadaca5.c
+++ zadaca5.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-inline int deliSo2(int);
+inline int DeliSo2(int);
 
 int main(int argc, char * argv[])
 {
 int brojac, prag = atoi(argv[1]);
 
 for (brojac = 0; brojac <= prag; brojac++)
-    printf("%d / 2 = %d\n", brojac, deliso2(brojac));
+    printf("%d / 2 = %d\n", brojac, DeliSo2(brojac));
 
 return 0;
 }
 
-int deliSo2(int arg)
+int DeliSo2(int arg)
 {
     return arg >> 1;
 }

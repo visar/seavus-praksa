70fa6cacc3714816d8e31d67a2d9e64d22fbb175
visar <visar.zejnullahu@gmail.com>
2012-07-06 00:08:59 +0200

    Vtor den

--- VtorDen/zadaca1.c
+++ VtorDen/zadaca1.c
@@ -7,90 +7,82 @@ void print(void *, size_t);
 
 int main()
 {
-    /* void * buffer1 = calloc(64,sizeof(void)); */
-    /* void * tmp = buffer1; */
-    char buffer2[64];
-    int buffer3[64];
-    long buffer4[64];
-    long long buffer5[64];
-    unsigned brojac;
-    srand(time(NULL));
+	char buffer2[64];
+	int buffer3[64];
+	long buffer4[64];
+	long long buffer5[64];
+	char buffer[] = {
+		0x23, 0xFF, 0x0D, 0x48, 0xEA, 0xDE, 0xBB, 0x8C
+	};
+	unsigned brojac;
+	srand(time(NULL));
 
-    /* for(brojac = 64; brojac; brojac--) */
-    /* { */
-    /*     *buffer1 = rand() * CHAR_MAX + 1; */
-    /*     buffer1++; */
-    /*     /\* buffer1[brojac] = rand() * CHAR_MAX + 1; *\/ */
-    /* } */
+	for(brojac = 64; brojac; brojac--)
+	{
+		buffer2[brojac] = (char)(rand() * CHAR_MAX + 1);
+	}
 
-    /* buffer1 = tmp; */
+	for(brojac = 64; brojac; brojac--)
+	{
+		buffer3[brojac] = (int)(rand() * INT_MAX + 1);
+	}
 
-    for(brojac = 64; brojac; brojac--)
-    {
-        buffer2[brojac] = (char)(rand() * CHAR_MAX + 1);
-    }
+	for(brojac = 64; brojac; brojac--)
+	{
+		buffer4[brojac] = (long)(rand() * LONG_MAX + 1);
+	}
 
-    for(brojac = 64; brojac; brojac--)
-    {
-        buffer3[brojac] = (int)(rand() * INT_MAX + 1);
-    }
-
-    for(brojac = 64; brojac; brojac--)
-    {
-        buffer4[brojac] = (long)(rand() * LONG_MAX + 1);
-    }
-
-    for(brojac = 64; brojac; brojac--)
-    {
-        buffer5[brojac] = (long long)(rand() * LLONG_MAX + 1);
-    }
+	for(brojac = 64; brojac; brojac--)
+	{
+		buffer5[brojac] = (long long)(rand() * LLONG_MAX + 1);
+	}
 
 
-    print((void *)buffer2, sizeof(buffer2));
-    print(buffer2, sizeof(buffer2));
-    print(buffer3, sizeof(buffer3));
-    print(buffer4, sizeof(buffer4));
-    print(buffer5, sizeof(buffer5));
-    /* free(buffer1); */
+	print((void *)buffer2, sizeof(buffer2));
+	print(buffer2, sizeof(buffer2));
+	print(buffer3, sizeof(buffer3));
+	print(buffer4, sizeof(buffer4));
+	print(buffer5, sizeof(buffer5));
+	print(buffer, sizeof(buffer));
 
-    return 0;
+	return 0;
 }
 
 void print(void * buffer, size_t size)
 {
-    unsigned char * tmp = (unsigned char *) buffer;
-    size_t len = size;
+	unsigned char * tmp = (unsigned char *) buffer;
+	size_t len = size;
 
-    while (len)
-    {
-        printf("0x%02x; ", *tmp);
-        tmp++;
-        len--;
-    }
+	while (len)
+	{
+		printf("0x%02X; ", *tmp);
+		tmp++;
+		len--;
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    unsigned short * tmp2 = (unsigned short *) buffer;
+	unsigned short * tmp2 = (unsigned short *) buffer;
 
-    len = size;
-    while (len)
-    {
-        printf("0x%04x; ", *tmp2);
-        tmp2++;
-        len -= 2;
-    }
+	len = size;
+	while (len)
+	{
+		printf("0x%04X; ", ((*tmp2 & 0x00ff) << 8) | ((*tmp2 & 0xff00) >> 8));
+		tmp2++;
+		len -= 2;
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    unsigned int * tmp3 = (unsigned int *) buffer;
+	unsigned int * tmp3 = (unsigned int *) buffer;
 
-    len = size;
-    while (len)
-    {
-        printf("0x%08x; ", *tmp3);
-        tmp3++;
-        len -= 4;
-    }
+	len = size;
+	while (len)
+	{
+		printf("0x%08X; ", (*tmp3 & 0x000000ff) << 24 | (*tmp3 & 0x0000ff00) << 8 | (*tmp3 & 0x00ff0000) >> 8| (*tmp3 & 0xff000000) >> 24);
+		tmp3++;
+		len -= 4;
+	}
 
-    putchar('\n');
+	putchar('\n');
 }
